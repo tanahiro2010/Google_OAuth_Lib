077624e85422ec223e0410191e562a0a0844d304
tanahiro2010 <herentongkegu087@gmail.com>
2025-12-22 17:32:48 +0900

    refactor: Userのエクスポートをコメント削除

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@
 export { default as Google } from "./base";
 export { default as OAuth2 } from "./lib/oauth";
 export { default as User } from "./lib/user";
-// export { default as User } from "./user"; // 実装後 ← コメント解除
 export type { GoogleProviderConfig, GoogleOAuthProvider } from "@/types";
